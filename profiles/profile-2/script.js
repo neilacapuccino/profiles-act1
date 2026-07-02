0d4867b84351a48c8bfb2130d2b5a2f9196e9060
Nherf0310 <nherfrossel.gempasao-18@cpu.edu.ph>
2026-07-02 08:56:58 +0800

    change to script.js

---
+++ profiles/profile-2/script.js
@@ -0,0 +1,5 @@
+function toggleLinks() {
+      const grid = document.getElementById('linkGrid');
+      const button = document.querySelector('.toggle-btn');
+      grid.classList.toggle('collapsed');
+    }
